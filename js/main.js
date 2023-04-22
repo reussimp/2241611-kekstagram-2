6fe135c6d34361a6acdab842fbb7bc22ce00b3f5
Anna Nelyubina <zefirkapro77@gmail.com>
2023-04-22 21:59:43 +0500

    Больше деталей исправленое

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ function getRandomPositiveInteger (a, b) {
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-//Функция, генерирующаю число, не повторяющаяся из диапазона включительно
+}
+//Функция, генерирующаю число,которое не повторяется из диапазона включительно
 function createRandom (min, max) {
   const previousValues = [];
 
@@ -23,6 +24,7 @@ function createRandom (min, max) {
 }
 const generateNumber = createRandom(1, 25);
 const generateNumberComment = createRandom(0, 1000);
+
 const MESSAGE = ['Всё отлично!',
   'В целом всё неплохо.Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -31,8 +33,9 @@ const MESSAGE = ['Всё отлично!',
   'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !',];
 const randomIndexMessage = getRandomPositiveInteger(0, MESSAGE.length - 1);
 
-const NAME = ['Марко', 'Серхио', 'Фернандо', 'Роберт', 'Ландо',];
+const NAME = ['Марко', 'Иван', 'Оскар', 'Фернандо', 'Ландо',];
 const randomIndexName = getRandomPositiveInteger(0, NAME.length - 1);
+
 const IMG = ['img/avatar - 1.svg', 'img/avatar - 2.svg', 'img/avatar - 3.svg', 'img/avatar - 4.svg', 'img/avatar - 5.svg', 'img/avatar - 6.svg'];
 const randomIndexImg = getRandomPositiveInteger(0, IMG.length-1);
 
@@ -56,3 +59,4 @@ const createDescriptionPhotoUser = () => ({
 const numberPhotoUsers = Array.from({length:25},createDescriptionPhotoUser);
 
 numberPhotoUsers();
+
