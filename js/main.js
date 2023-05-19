9225e9c2bb21364b9b5920dec04c94f9bc28c976
Anna Nelyubina <zefirkapro77@gmail.com>
2023-05-20 00:26:07 +0500

    правда или действие

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-import {renderPhotos} from './rendering.js';
-import {onPictureClick} from './zoom.js';
+import {showPhoto} from './rendering.js';
+import './new photo.js';
+import './validation-form.js';
+import {getData} from './api.js';
+import {showError} from './error.js';
+
+getData((pictures) => {
+  showPhoto(pictures);
+}, showError);
--- js/rendering.js
+++ js/rendering.js
@@ -1,30 +1,38 @@
-import { onPictureClick } from './zoom.js';
-const userPhotosTemplate = document.querySelector('#picture').content.querySelector('.picture');
+import {openBigPicture} from './zoom.js';
+
+const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const photoConteiner = document.querySelector('.pictures');
-const createPhoto = (photo) => {
-  const photoElement = userPhotosTemplate.cloneNode(true);
-  const photoImg = photoElement.querySelector('.picture__img');
+const photoListFragment = document.createDocumentFragment();
 
-  photoImg.src = photo.url;
-  photoImg.alt = photo.description;
-  photoElement.querySelector('.picture__likes').textContent = photo.likes;
-  photoElement.querySelector('.picture__comments').textContent = photo.comments.length;
-  photoElement.addEventListener('click', (evt) => {
+let pictures = [];
+
+const showPhoto = (photos) => {
+  pictures = photos;
+  photos.forEach(({url, comments, likes}, index) => {
+    const photoElement = photoTemplate.cloneNode(true);
+    photoElement.querySelector('.picture__img').setAttribute('photo-index', index);
+    photoElement.querySelector('.picture__img').src = url;
+    photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.querySelector('.picture__comments').textContent = comments.length;
+    photoListFragment.appendChild(photoElement);
+  });
+
+  photoConteiner.appendChild(photoListFragment);
+};
+
+const hidePictures = () => {
+  photoConteiner.querySelectorAll('.picture').forEach((photoElement) => {
+    photoElement.remove();
+  });
+};
+
+const onPhotoListClick = function (evt) {
+  if (evt.target.nodeName === 'IMG') {
     evt.preventDefault();
-    onPictureClick(photo);
-  });
-
-  return photoElement;
+    openBigPicture(pictures[evt.target.getAttribute('photo-index')]);
+  }
 };
 
-const renderPhotos = (data) => {
-  const fragment = document.createDocumentFragment();
-  data.forEach((item) => {
-    const element = createPhoto(item);
-    fragment.append(element);
-  });
-  photoConteiner.innerHTML = '';
-  photoConteiner.append(fragment);
-};
+photoConteiner.addEventListener('click', onPhotoListClick);
 
-export { renderPhotos };
+export {showPhoto, hidePictures};
